4cdd6074e5978066d89156f7df7069f74e6a10d9
Hassan <hasantahir214@gmail.com>
2024-09-13 01:44:56 +0500

    create CRUD

--- routes/todos.js
+++ routes/todos.js
@@ -60,4 +60,86 @@ router.post(
     }
 );
 
+router.delete("/:id", auth.authenticateToken, async (req, res) => {
+    const todo_id = req.params.id;
+    let connection = await pool.getConnection();
+
+    try {
+        deleteQuery =
+            "delete from `todo` where id = ?"
+        const [deleteResult] = await connection.query(deleteQuery, [todo_id]);
+        // Send a success response
+        logger.info("Todo deleted successfully");
+        return res.status(200).json({ message: "Todo delete successfully" });
+    } catch (error) {
+        // Log the original error and send an error response
+        logger.error("delete todo error is:", error);
+        return res.status(400).json({ message: error.message });
+    }
+})
+
+
+router.put(
+    "/:id",
+    auth.authenticateToken,
+    async (req, res) => {
+        const signedin_user = res.locals.id;
+        const todo_id = req.params.id;
+        const r = req.body;
+        let connection = await pool.getConnection();
+        try {
+            query =
+                "UPDATE `todo` SET `title` = ?, `due_date` = ?, `priority` = ?, `updated_at` = now(), `updated_by` = ? WHERE (`id` = ?)";
+
+            const [result] = await connection.query(query, [
+                r.title,
+                r.due_date,
+                r.priority,
+                signedin_user,
+                todo_id
+            ]);
+            logger.info("Todo updatad successfully");
+            return res.status(200).json({ message: "Todo updated successfully" });
+
+        } catch (error) {
+            // Log the original error and send an error response
+            logger.error("Todo update error is:", error);
+            return res.status(400).json({ message: error.message });
+
+        }
+    }
+
+);
+
+router.put(
+    "/status/:id",
+    auth.authenticateToken,
+    async (req, res) => {
+        const signedin_user = res.locals.id;
+        const todo_id = req.params.id;
+        const r = req.body;
+        let connection = await pool.getConnection();
+        try {
+            query =
+                "UPDATE `todo` SET `status` = ?, `updated_at` = now(), `updated_by` = ? WHERE (`id` = ?)";
+
+            const [result] = await connection.query(query, [
+                r.status,
+                signedin_user,
+                todo_id
+            ]);
+            logger.info("Todo Status updatad successfully");
+            return res.status(200).json({ message: "Todo Status updated successfully" });
+
+        } catch (error) {
+            // Log the original error and send an error response
+            logger.error("Todo Status update error is:", error);
+            return res.status(400).json({ message: error.message });
+
+        }
+    }
+
+);
+
+
 module.exports = router;
